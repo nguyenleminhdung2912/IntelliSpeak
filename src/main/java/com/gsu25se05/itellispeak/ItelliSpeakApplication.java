d804629cf8a3272a53b9053751849f97210edb68
nguyenleminhdung2912 <nguyenleminhdung2912@gmail.com>
2025-05-18 14:15:25 +0700

    init

---
+++ src/main/java/com/gsu25se05/itellispeak/ItelliSpeakApplication.java
@@ -0,0 +1,13 @@
+package com.gsu25se05.itellispeak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItelliSpeakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItelliSpeakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gsu25se05/itellispeak/ItelliSpeakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsu25se05.itellispeak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItelliSpeakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
