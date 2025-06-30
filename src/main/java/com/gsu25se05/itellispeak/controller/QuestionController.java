c4503d3227d38e43d9f1f646c0ac1d565d8638bc
nguyenleminhdung2912 <nguyenleminhdung2912@gmail.com>
2025-06-30 08:04:48 +0700

    Controller: Fix mapping for QuestionController

--- src/main/java/com/gsu25se05/itellispeak/controller/QuestionController.java
+++ src/main/java/com/gsu25se05/itellispeak/controller/QuestionController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/topic")
+@RequestMapping("/question")
 @CrossOrigin("**")
 @SecurityRequirement(name = "api")
 public class QuestionController {
