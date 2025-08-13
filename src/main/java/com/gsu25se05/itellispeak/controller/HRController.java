c0532598524decaacc016939da28711e09f31a13
nguyenleminhdung2912 <nguyenleminhdung2912@gmail.com>
2025-08-13 22:04:25 +0700

    add create session for HR Controller

--- src/main/java/com/gsu25se05/itellispeak/controller/HRController.java
+++ src/main/java/com/gsu25se05/itellispeak/controller/HRController.java
@@ -4,9 +4,14 @@ package com.gsu25se05.itellispeak.controller;
 import com.gsu25se05.itellispeak.dto.Response;
 import com.gsu25se05.itellispeak.dto.hr.HRRequestDTO;
 import com.gsu25se05.itellispeak.dto.hr.HRResponseDTO;
+import com.gsu25se05.itellispeak.dto.interview_session.InterviewSessionDTO;
+import com.gsu25se05.itellispeak.entity.InterviewSession;
 import com.gsu25se05.itellispeak.service.HRService;
+import com.gsu25se05.itellispeak.service.InterviewSessionService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,9 +21,11 @@ import org.springframework.web.bind.annotation.*;
 public class HRController {
 
     private final HRService hrService;
+    private final InterviewSessionService interviewSessionService;
 
-    public HRController(HRService hrService) {
+    public HRController(HRService hrService, InterviewSessionService interviewSessionService) {
         this.hrService = hrService;
+        this.interviewSessionService = interviewSessionService;
     }
 
     @PostMapping("/apply")
@@ -29,5 +36,12 @@ public class HRController {
     @GetMapping("/application/status")
     public Response<HRResponseDTO> getApplicationStatus() {
         return hrService.checkHRApplicationStatus();
+    }
+
+    @PostMapping("/create")
+    @Operation(summary = "Tạo interview session")
+    public ResponseEntity<Response<InterviewSession>> create(@RequestBody InterviewSessionDTO interviewSessionDTO) {
+        InterviewSession session = interviewSessionService.saveFromHR(interviewSessionDTO);
+        return ResponseEntity.ok(new Response<>(200, "Interview session created", session));
     }
 }
